da9aacb1c829c23ba31ce4683733c14f9c22d1ee
kmiqbal19 <km.iqbal01@gmail.com>
2022-05-31 20:09:32 +0200

    updated headers cors

--- index.js
+++ index.js
@@ -20,7 +20,12 @@ const postRouter = require("./routes/postRouter");
 // Create Express Application
 const app = express();
 // CORS
-app.use(cors({ credentials: true, optionsSuccessStatus: true }));
+app.use(
+  cors({
+    credentials: true,
+    optionsSuccessStatus: true,
+  })
+);
 // Configure .env to process.env
 dotenv.config({ path: "./.env" });
 // Connection to Database (MongoDB)
@@ -135,6 +140,14 @@ app.use(globalErrorHandler);
 // } else {
 // }
 app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
   res.send("Backend is running!");
 });
 
